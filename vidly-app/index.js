9d95f263228b93273abb727d608ba7244a9821de
Hannah Roe <hannahdroe@gmail.com>
2018-05-22 16:04:46 -0600

    Added mongoose Genres Api in vidly app

--- vidly-app/index.js
+++ vidly-app/index.js
@@ -1,8 +1,14 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
+
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const home = require('./routes/home');
+
+mongoose.connect('mongodb://localhost/vidly-app')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB', err))
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
