484fd993cbebc4b2c65914f6e033831214c0d966
Hannah Roe <hannahdroe@gmail.com>
2018-07-26 15:43:12 -0600

    added auth path

--- vidly-app/index.js
+++ vidly-app/index.js
@@ -9,7 +9,7 @@ const home = require('./routes/home');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
-
+const auth = require('./routes/auth');
 mongoose.connect('mongodb://localhost/vidly-app')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB', err))
@@ -23,7 +23,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
-
+app.use('/api/auth', auth);
 
 //setting up port
 const port = process.env.PORT || 3000;
