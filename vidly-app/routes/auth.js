7f764641b13c9e1529f55611d67d0d72af29c3f8
Hannah Roe <hannahdroe@gmail.com>
2018-07-26 18:25:04 -0600

    added jwt, config for env variable, and header to store jwt on client

--- vidly-app/routes/auth.js
+++ vidly-app/routes/auth.js
@@ -1,9 +1,11 @@
+const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const Joi = require('joi');
+const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 
 
@@ -16,9 +18,11 @@ router.post('/', async (req, res) =>{
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
     if (!validPassword)return res.status(400).send('Invalid email or password');
-    
-    res.send(true);
+    // genterate jwt
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
+    res.send(token);
 });
+
 function validate(req){
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
--- vidly-app/routes/users.js
+++ vidly-app/routes/users.js
@@ -4,7 +4,8 @@ const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const { User, validate } = require('../models/user');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 router.post('/', async (req, res) =>{
     const { error } = validate(req.body);
@@ -17,10 +18,10 @@ router.post('/', async (req, res) =>{
     
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
-
     await user.save();
+    const token = jwt.sign({_id: user._id}, config.get('jwtPrivateKey'));
 
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 
  });
 
